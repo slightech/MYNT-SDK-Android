948c224d696b9162bd1dde3c89f692aa5071fae0
John Zhao <johnzhao@slightech.com>
2016-04-09 11:24:17 +0800

    Add get service kit method

--- demo/mynt-sdk-demo/app/src/main/java/com/slightech/mynt/sdk/demo/util/KitUtils.java
+++ demo/mynt-sdk-demo/app/src/main/java/com/slightech/mynt/sdk/demo/util/KitUtils.java
@@ -1,6 +1,7 @@
 package com.slightech.mynt.sdk.demo.util;
 
 import android.app.Activity;
+import android.content.Context;
 import android.view.View;
 
 public class KitUtils {
@@ -13,5 +14,15 @@ public class KitUtils {
     @SuppressWarnings({ "unchecked", "UnusedDeclaration" })
     public static <T extends View> T findById(Activity activity, int id) {
         return (T) activity.findViewById(id);
+    }
+
+    /**
+     * Return the handle to a system-level service by name.
+     *
+     * @see Context#getSystemService(String)
+     */
+    @SuppressWarnings({ "unchecked", "UnusedDeclaration" })
+    public static <T> T getService(Context context, String name) {
+        return (T) context.getSystemService(name);
     }
 }
